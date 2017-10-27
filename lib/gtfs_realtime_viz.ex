0b0e6e41e5c36d556f463cf29f717e6a302b7f8b
Gabe Durazo <gabe@durazo.us>
2017-10-27 13:55:54 -0500

    GTFSRealtimeViz.visualize/2 takes raw data and makes an HTML table

--- lib/gtfs_realtime_viz.ex
+++ lib/gtfs_realtime_viz.ex
@@ -1,9 +1,31 @@
 defmodule GTFSRealtimeViz do
-  use Protobuf, from: Path.expand("../config/gtfs-realtime.proto", __DIR__)
   @moduledoc """
   Parses GTFS Realtime PB file and generates a nice visualization of it.
 
   $ iex -S mix
   iex(1)> "filename" |> File.read! |> GTFSRealtimeViz.FeedMessage.decode
   """
+
+  use Protobuf, from: Path.expand("../config/gtfs-realtime.proto", __DIR__)
+
+  require EEx
+  EEx.function_from_file :def, :gen_html, "lib/viz.eex", [:assigns], [engine: Phoenix.HTML.Engine]
+
+  def visualize(raw, filename) do
+    ladder_config = %{
+      "Mattapan" => [
+        {"70261", "Ashmont"},
+        {"70268", "Milton"},
+      ]
+    }
+    data = GTFSRealtimeViz.FeedMessage.decode(raw) #TODO: handle failure
+    vehicles = Enum.map(data.entity, & &1.vehicle) #TODO: handle if no entity, or not all vehicles
+
+    content =
+      [vehicles: vehicles]
+      |> gen_html
+      |> Phoenix.HTML.safe_to_string
+
+    File.write!(filename, content)
+  end
 end
