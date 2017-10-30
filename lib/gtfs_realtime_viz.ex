ba8a530268d4607eefead9169785cc78c0adf338
Gabe Durazo <gabe@durazo.us>
2017-10-30 12:58:06 -0400

    convert to app with supervised genserver

--- lib/gtfs_realtime_viz.ex
+++ lib/gtfs_realtime_viz.ex
@@ -3,26 +3,22 @@ defmodule GTFSRealtimeViz do
   Parses GTFS Realtime PB file and generates a nice visualization of it.
 
   $ iex -S mix
-  iex(1)> "filename" |> File.read! |> GTFSRealtimeViz.FeedMessage.decode
+  iex(1)> "filename.pb" |> File.read! |> GTFSRealtimeViz.new_message
+  iex(2)> GTFSRealtimeViz.visualize("output.html")
   """
 
-  use Protobuf, from: Path.expand("../config/gtfs-realtime.proto", __DIR__)
+  alias GTFSRealtimeViz.State
 
   require EEx
   EEx.function_from_file :def, :gen_html, "lib/viz.eex", [:assigns], [engine: Phoenix.HTML.Engine]
 
-  def visualize(raw, filename) do
-    ladder_config = %{
-      "Mattapan" => [
-        {"70261", "Ashmont"},
-        {"70268", "Milton"},
-      ]
-    }
-    data = GTFSRealtimeViz.FeedMessage.decode(raw) #TODO: handle failure
-    vehicles = Enum.map(data.entity, & &1.vehicle) #TODO: handle if no entity, or not all vehicles
+  def new_message(raw) do
+    State.new_data(raw)
+  end
 
+  def visualize(filename) do
     content =
-      [vehicles: vehicles]
+      [vehicles: State.vehicles()]
       |> gen_html
       |> Phoenix.HTML.safe_to_string
 
