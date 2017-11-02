d41239deded1867bfb0a54140b67794c656364cb
Gabe Durazo <gabe@durazo.us>
2017-11-02 11:10:24 -0400

    Add a test

--- test/gtfs_realtime_viz_test.exs
+++ test/gtfs_realtime_viz_test.exs
@@ -1,8 +1,43 @@
 defmodule GTFSRealtimeVizTest do
   use ExUnit.Case
-  doctest GTFSRealtimeViz
 
-  test "greets the world" do
-    assert GTFSRealtimeViz.hello() == :world
+  alias GTFSRealtimeViz.Proto
+
+  test "visualizes a file" do
+    data = %Proto.FeedMessage{
+      header: %Proto.FeedHeader{
+        gtfs_realtime_version: "1.0",
+      },
+      entity: [
+        %Proto.FeedEntity{
+          id: "123",
+          is_deleted: false,
+          vehicle: %Proto.VehiclePosition{
+            trip: %Proto.TripDescriptor{
+              trip_id: "this_is_the_trip_id",
+              route_id: "this_is_the_route_id",
+              direction_id: 0,
+            },
+            vehicle: %Proto.VehicleDescriptor{
+              id: "this_is_the_vehicle_id",
+              label: "this_is_the_vehicle_label",
+            },
+            position: %Proto.Position{
+              latitude: 0.00,
+              longitude: 0.00,
+            },
+            stop_id: "this_is_the_stop_id",
+          }
+        }
+      ]
+    }
+
+    raw = Proto.FeedMessage.encode(data)
+
+    GTFSRealtimeViz.new_message(raw, "this is the test data")
+    viz = GTFSRealtimeViz.visualize()
+
+    assert viz =~ "this is the test data"
+    assert viz =~ "this_is_the_vehicle_id"
   end
 end
